b6be991d1a9baa2561cbcaafc6232afbe018f646
Himanshu Gupta <h23himanshu@gmail.com>
2025-06-27 22:31:08 +0530

    add macOS target for calculator logic

---
+++ CalculatorLogic/CalculatorLogic.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorLogic.swift
+//  CalculatorLogic
+//
+//  Created by Himanshu on 27/06/25.
+//
+
+import Foundation
+
---
+++ CalculatorLogicTests/CalculatorLogicTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorLogicTests.swift
+//  CalculatorLogicTests
+//
+//  Created by Himanshu on 27/06/25.
+//
+
+import Testing
+@testable import CalculatorLogic
+
+struct CalculatorLogicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
