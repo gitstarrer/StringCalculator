89454f8f71d6a6d8d581184b9adddee3f08a1cff
Himanshu Gupta <h23himanshu@gmail.com>
2025-06-27 22:09:27 +0530

    Initial Commit

---
+++ StringCalculator/StringCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorApp.swift
+//  StringCalculator
+//
+//  Created by Himanshu on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
