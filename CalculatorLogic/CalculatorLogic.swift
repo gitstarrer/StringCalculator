b210646b6094962a56799e70c210ca3ec9fc7c05
Himanshu Gupta <h23himanshu@gmail.com>
2025-06-28 10:22:40 +0530

    refactor add method to move fetching the delimiter logic to a separate function

--- CalculatorLogic/CalculatorLogic.swift
+++ CalculatorLogic/CalculatorLogic.swift
@@ -11,17 +11,22 @@ struct CalculatorLogic {
     
     func add(_ string: String) -> Int {
         guard !string.isEmpty else { return 0 }
-        var delimiter: Character = ","
-        if string.hasPrefix("//"),
-           let newlineIndex = string.firstIndex(of: "\n") {
-            delimiter = string[string.index(before: newlineIndex)]
-        }
         
+        let delimiter = getDelimiter(string)
         let numbers = string.split{ $0 == delimiter || $0 == "\n" }
         var sum = 0
         for number in numbers {
             sum += Int(number) ?? 0
         }
         return sum
+    }
+    
+    func getDelimiter(_ string: String) -> Character {
+        var delimiter: Character = ","
+        if string.hasPrefix("//"),
+           let newlineIndex = string.firstIndex(of: "\n") {
+            delimiter = string[string.index(before: newlineIndex)]
+        }
+        return delimiter
     }
 }
